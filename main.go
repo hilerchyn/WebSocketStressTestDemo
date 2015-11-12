84aed20fba4276711713e3210bda92f470ff044a
monstarlab-qingdao <monstarlab-qingdao@Tao.local>
2015-11-12 10:34:20 +0800

    添加自定义命令参数

--- main.go
+++ main.go
@@ -14,19 +14,37 @@ import (
 
 	"github.com/gorilla/websocket"
 	"sync"
+	"strings"
 )
 
-var addr = flag.String("addr", "localhost:8000", "http service address")
+var addr = flag.String("addr", "localhost:8000", "服务器连接地址")
+var con = flag.Int("con", 40, "并发总数")
+var timeStep = flag.Int("timeStep", 1, "重连的时间间隔,以秒为单位")
+
+var tickets chan int
 
 func main() {
+
 	flag.Parse()
 	log.SetFlags(0)
 
+
+	tickets = make(chan int, *con)
+
 	var wg sync.WaitGroup
 
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		go connection(&wg)
+	for {
+
+		select {
+		case tickets<-1:
+			wg.Add(1)
+			log.Println("tickets capacity:", cap(tickets))
+			log.Println("create connection")
+			go connection(&wg)
+		case <-time.After(time.Duration(100) * time.Millisecond):
+
+		}
+
 	}
 
 	wg.Wait()
@@ -35,8 +53,11 @@ func main() {
 
 func connection(wg *sync.WaitGroup) {
 
-	//u := url.URL{Scheme: "ws", Host: *addr, Path: "/socket.io/?EIO=3&transport=websocket"}
-	u := "ws://localhost:8000/socket.io/?EIO=3&transport=websocket"
+	ticker := time.NewTicker(time.Duration(*timeStep) * time.Second)
+	defer ticker.Stop()
+
+	//u := url.URL{Scheme: "ws", Host: *addr, Path: /socket.io/?EIO=3&transport=websocket"}
+	u := "ws://"+*addr+"/socket.io/?EIO=3&transport=websocket"
 	//u := url.URL{Scheme: "ws", Host: *addr, Path: "/socket.io/1/?EIO=2&transport=polling&t=1446710999349"}
 	log.Printf("connecting to %s", u)
 
@@ -55,17 +76,26 @@ func connection(wg *sync.WaitGroup) {
 				wg.Done()
 				break
 			}
-			log.Printf("recv: %s", message)
+			//log.Printf("recv: %s", message)
+			if strings.Contains(string(message), "login_resp") {
+
+				ticker.Stop()
+				c.Close()
+				wg.Done()
+				<-tickets
+
+				log.Println("done")
+				break
+			}
 		}
 	}()
 
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+
 
 	for t := range ticker.C {
 		//err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
 		//req	__NSCFString *	@"42[\"login\",{\"client_id\":\"1\",\"token\":\"a433709204fadb3f97acb386cf3bb1b9\",\"client_type\":\"user\"}]"	0x00007fcc78eac2f0
-		log.Println(t.String())
+		t.String()
 		//err := c.WriteMessage(websocket.TextMessage, []byte("42[\"message\",\"3:2\"]"))
 		err := c.WriteMessage(websocket.TextMessage, []byte("42[\"login\",{\"client_id\":\"1\",\"token\":\"a433709204fadb3f97acb386cf3bb1b9\",\"client_type\":\"user\"}]"))
 		if err != nil {
