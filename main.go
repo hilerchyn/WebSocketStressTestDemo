4b26e3e51e6fabac12b3d8e1a2dd884c1383d40d
monstarlab-qingdao <monstarlab-qingdao@Tao.local>
2015-11-12 11:24:22 +0800

    添加自定义命令参数

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ import (
 
 var addr = flag.String("addr", "localhost:8000", "服务器连接地址")
 var con = flag.Int("con", 40, "并发总数")
-var timeStep = flag.Int("timeStep", 1, "重连的时间间隔,以秒为单位")
+var interval = flag.Int("interval", 1, "重连的时间间隔,以秒为单位")
+var content = flag.String("content", "42[\"login\",{\"client_id\":\"1\",\"token\":\"a433709204fadb3f97acb386cf3bb1b9\",\"client_type\":\"user\"}]", "向服务器发送的数据")
 
 var tickets chan int
 
@@ -53,7 +54,7 @@ func main() {
 
 func connection(wg *sync.WaitGroup) {
 
-	ticker := time.NewTicker(time.Duration(*timeStep) * time.Second)
+	ticker := time.NewTicker(time.Duration(*interval) * time.Second)
 	defer ticker.Stop()
 
 	//u := url.URL{Scheme: "ws", Host: *addr, Path: /socket.io/?EIO=3&transport=websocket"}
@@ -97,7 +98,7 @@ func connection(wg *sync.WaitGroup) {
 		//req	__NSCFString *	@"42[\"login\",{\"client_id\":\"1\",\"token\":\"a433709204fadb3f97acb386cf3bb1b9\",\"client_type\":\"user\"}]"	0x00007fcc78eac2f0
 		t.String()
 		//err := c.WriteMessage(websocket.TextMessage, []byte("42[\"message\",\"3:2\"]"))
-		err := c.WriteMessage(websocket.TextMessage, []byte("42[\"login\",{\"client_id\":\"1\",\"token\":\"a433709204fadb3f97acb386cf3bb1b9\",\"client_type\":\"user\"}]"))
+		err := c.WriteMessage(websocket.TextMessage, []byte(*content))
 		if err != nil {
 			log.Println("write:", err)
 			wg.Done()
